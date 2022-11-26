e88af86b004e11b7b3a7073cf9803c8df456d59d
Keith Ma <keithchezzy@gmail.com>
2022-11-25 17:15:01 -0800

    Event

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChange() {
+        console.log('Entering password...');
+      }
     return (
-        <div></div>
+        <div>
+            <input
+        type="password"
+        name="password"
+        onChange={handleChange}
+        placeholder="password"
+      />
+        </div>
     )
 }
 
